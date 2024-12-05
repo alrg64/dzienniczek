85b27b5709bab0c04f58087cc0bfeb2dcb302e17
student <student@B2SALA16>
2024-12-05 11:19:16 +0100

    Dodaj pliki projektów.

---
+++ dzienniczek/DodajPrzedmiot.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dzienniczek
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DodajPrzedmiot.xaml
+    /// </summary>
+    public partial class DodajPrzedmiot : UserControl
+    {
+        public DodajPrzedmiot()
+        {
+            InitializeComponent();
+        }
+
+        private void dodajPrzedmiot_Click(object sender, RoutedEventArgs e)
+        {
+            string nazwa = nazwaPrzedmiotu.Text;
+            string lancuchPolaczenia = "DataSource=sample.db;Version=3;";
+            if (nazwa != null)
+            {
+                using SQLiteConnection polaczenie = new SQLiteConnection(lancuchPolaczenia);
+                {
+                    try
+                    {
+                        polaczenie.Open();
+                        string utworzenieBazy = "CREATE TABLE IF NOT EXISTS przedmioty (id INTEGER PRIMARY KEY AUTOINCREMENT, nazwa TEXT NOT NULL, srednia REAL NOT NULL);";
+                        using (SQLiteCommand komenda = new SQLiteCommand(utworzenieBazy, polaczenie))
+                        {
+                            komenda.ExecuteNonQuery();
+                        }
+                        string zapytanie = $"INSERT INTO przedmioty (nazwa, srednia) VALUES ('{nazwa}', 0)";
+                        using (SQLiteCommand komenda = new SQLiteCommand(zapytanie, polaczenie))
+                        {
+                            komenda.ExecuteNonQuery();
+                            komunikat.Text = "Dodano przedmiot";
+                            Window mainWindow = new MainWindow();
+                            mainWindow.Show();
+                            Application.Current.MainWindow.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        komunikat.Text = "Błąd";
+                    }
+                }
+            }
+            else
+            {
+                komunikat.Text = "Wprowadź nazwę przedmiotu";
+            }
+        }
+    }
+}
---
+++ dzienniczek/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Data.SQLite;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dzienniczek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> przedmioty = new List<string>();
+            string lancuchPolaczenia = "DataSource=sample.db;Version=3;";
+            using SQLiteConnection polaczenie = new SQLiteConnection(lancuchPolaczenia);
+            {
+                try
+                {
+                    polaczenie.Open();
+                    string wyswietlPrzedmioty = "SELECT * FROM przedmioty;";
+                    using (SQLiteCommand komenda = new SQLiteCommand(wyswietlPrzedmioty, polaczenie))
+                    {
+                        using (SQLiteDataReader reader = komenda.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    int id = Convert.ToInt32(reader[0]);
+                                    string nazwa = Convert.ToString(reader[1]);
+                                    double srednia = Convert.ToDouble(reader[2]);
+                                    TextBlock textBlock = new TextBlock();
+                                    textBlock.Text = nazwa;
+                                    textBlock.Foreground = Brushes.Chocolate;
+                                    panel.Children.Add(textBlock);
+                                    przedmioty.Add(nazwa);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    komunikat.Text = "Błąd\n" + ex.Message;
+                }
+            }
+        }
+
+        private void dodawaniePrzedmiotu_Click(object sender, RoutedEventArgs e)
+        {
+            dodawaniePrzedmiotuPopup.IsOpen = true;
+        }
+
+        private void usuwaniePrzedmiotu_Click(object sender, RoutedEventArgs e)
+        {
+            usuwaniePrzedmiotuPopup.IsOpen = true;
+        }
+    }
+}
